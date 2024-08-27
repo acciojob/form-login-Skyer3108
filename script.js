ffee846a2823d8077c34c42e2e5dbdd74395e96e
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 15:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+   
+    event.preventDefault();
 
+    
+    const firstName = document.querySelector('input[name="fname"]').value;
+    const lastName = document.querySelector('input[name="lname"]').value;
+
+   
+    alert(`${firstName} ${lastName}`);
 }
+
+
+window.onload = function() {
+    document.getElementById('form1').onsubmit = getFormvalue;
+};
